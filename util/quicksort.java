c5e15f0cdbf4eb73d1e52c8de35a23868b728370
wy4515 <y.wu4515@gmail.com>
2014-12-19 02:35:57 -0500

    Create quicksort.java
    quick sort algorithm

---
+++ util/quicksort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class QuickSort {
+  public QuickSort(int[] array) {
+    QSort(array,0,array.length-1);
+  }
+  
+  private void QSort(int[] array, int l, int r) {
+    while(l<r) {
+      int pivot = partition(array, l, r);
+      QSort(array, l, pivot-1);
+      // does not recursively call QSort, for stack sake
+      l = pivot+1;
+    }
+  }
+  
+  private int partition(int[] array, int l, int r) {
+    int pivot = array[l];
+    while (l<r) {
+      // order matters
+      while(l<r && array[r]>=pivot)
+        r--;
+      // just replace, no need for swap  
+      array[r] = array[l];
+      while(l<r && array[l]<=pivot)
+        l++;
+      array[r] = array[r];
+    }
+    array[l] = pivot;
+    return l;
+  }
+}
