7baca53186270b2e45b9a503d827e2cc78980d81
wy4515 <y.wu4515@gmail.com>
2015-01-11 14:56:11 -0500

    Create 2sum.java

---
+++ 2sum.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int size = numbers.length;
+        int[] sortedArray = Arrays.sort(numbers);
+        int left = 0, right = size-1;
+        while (left<right) {
+            if (sortedArray[left]+sortedArray[right] > target)
+                right--;
+            else if (sortedArray[left]+sortedArray[right] < target) 
+                left++;
+            else {
+                int[] idx = {left+1, right+1};
+                return idx;
+            } 
+        }
+        return null
+    }
+}
