270b4d0681e605fd15cb1980ee150d46e88086ec
wy4515 <y.wu4515@gmail.com>
2015-01-10 21:08:16 -0500

    Update queue_linkedlist.java

--- data-structure/queue_linkedlist.java
+++ data-structure/queue_linkedlist.java
@@ -3,6 +3,11 @@ public class Queue_LL {
     public void enqueue(Node n) {
         queue.push(n);
     }
+    /*
+        linkedlist input: 1 - 3 - 7;
+        allocation : {7, 3, 1}, stack-like order
+        therefore, wanna get 1 : take getLast()
+    */
     public Node dequeue() {
         Node n = queue.getLast();       // getLast() HERE!
         queue.remove(queue.size()-1);   // manually remove!
