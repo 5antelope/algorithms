0b4816332ab0cee2a1ff93f75dacaaea78deca97
wy4515 <y.wu4515@gmail.com>
2015-01-10 20:26:22 -0500

    Create stack.java

---
+++ data-structure/stack.java
@@ -0,0 +1,29 @@
+Class Node {
+  int val;
+  Node next;
+  Node (int val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+// use linked list for stack / queue
+Class Stack {
+  Node top;
+  public Stack(Node n) {
+    top = n;
+  }
+  public Node peek () {
+    Node p = top;
+    top = p.next;
+    return p;
+  }
+  public Node pop () {
+    Node p = top;
+    top = p.next;
+    return p;
+  }
+  public void push(Node node) {
+    top.next = node;
+    top = node;
+  }
+}
