aa04c6cfed91babbbdf66da52158c26eaadf3bf7
wy4515 <y.wu4515@gmail.com>
2015-01-11 22:28:09 -0500

    Update MergeKSortedList.java

--- MergeKSortedList.java
+++ MergeKSortedList.java
@@ -30,3 +30,30 @@ public class Solution {
         return header.next;
     }
 }
+
+// 2nd. solution, a little slower but much less extra space used
+public class Solution {
+    ListNode node = new ListNode(0);
+    ListNode header = node;
+    public ListNode mergeKLists(List<ListNode> lists) {
+        int len = lists.size();
+        if (len<1)  return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+            public int compare(ListNode n1, ListNode n2) {
+                return n1.val-n2.val;
+            }
+        });
+        for (ListNode n: lists) {
+            if (n!=null)
+                queue.offer(n);
+        }    
+        while (queue.size()>0) {
+            ListNode tmp = queue.poll();
+            node.next = tmp;
+            node = node.next;
+            if (tmp.next!=null)
+                queue.offer(tmp.next);
+        }
+        return header.next;
+    }
+}
