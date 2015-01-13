b8854d4a6c9987a53a81eb15b2b03ba2138a69a0
wy4515 <y.wu4515@gmail.com>
2015-01-12 22:51:30 -0500

    Line 23: java.lang.ArrayIndexOutOfBoundsException

---
+++ 4sum.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        int n = num.length;
+        if (n<4)    return null;
+        Arrays.sort(num);
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        for (int i=0; i<n-3; i++) {
+            for (int j=i+1; j<n-2; j++) {
+                if (num[j]==num[j-1])   continue;
+                int l = j+1;
+                int r = n-1;
+                while (l<r) {
+                    int sum = num[i]+num[j]+num[l]+num[r];
+                    if (sum>target) r--;
+                    else if (sum<target)    l++;
+                    else {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(num[i]);
+                        list.add(num[j]);
+                        list.add(num[l++]);
+                        list.add(num[r--]);
+                        lists.add(list);
+                        while (l>r && num[l]==num[l-1]) l++;
+                        while (l>r && num[r]==num[r+1]) r--;
+                    }
+                }
+            }
+        }
+        return lists;
+    }
+}
