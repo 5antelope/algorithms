b30d71fb9beebde12542fa805982ca42afe020ff
wy4515 <y.wu4515@gmail.com>
2015-01-12 23:07:43 -0500

    AC

--- 4sum.java
+++ 4sum.java
@@ -5,8 +5,9 @@ public class Solution {
         Arrays.sort(num);
         List<List<Integer>> lists = new ArrayList<List<Integer>>();
         for (int i=0; i<n-3; i++) {
+            if (i>0 && num[i]==num[i-1])  continue;
             for (int j=i+1; j<n-2; j++) {
-                if (num[j]==num[j-1])   continue;
+                if (j>i+1 && num[j]==num[j-1])   continue;
                 int l = j+1;
                 int r = n-1;
                 while (l<r) {
@@ -20,8 +21,8 @@ public class Solution {
                         list.add(num[l++]);
                         list.add(num[r--]);
                         lists.add(list);
-                        while (l>r && num[l]==num[l-1]) l++;
-                        while (l>r && num[r]==num[r+1]) r--;
+                        while (l<r && num[l]==num[l-1]) l++;
+                        while (l<r && num[r]==num[r+1]) r--;
                     }
                 }
             }
