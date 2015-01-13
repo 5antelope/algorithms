dd87d6508cc50a3ab4a5354fdf292c629c91c036
wy4515 <y.wu4515@gmail.com>
2015-01-13 10:42:53 -0500

    AC

--- BestTimeToBuyAndSellStock|||.java
+++ BestTimeToBuyAndSellStock|||.java
@@ -1,20 +1,22 @@
 public class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int max=0;
-        int[][] A = new int[n+1][n+1];
+        if (n<2) return 0;
+        int[] max1=new int[n+1];
+        int[] max2=new int[n+1];
+        int minTmp = prices[0];
         for (int i=1; i<=n; i++) {
-            for (int j=i+1; j<=n; j++) {
-                if (j<n) {
-                    int diff = prices[j-1]-prices[j];
-                    A[i][j] = Math.max(A[i][j-1], A[i][j-1]+diff);
-                }
-                else 
-                    A[i][j] = A[i][j-1];
-            }
+            minTmp = Math.min(minTmp, prices[i-1]);
+            max1[i] = Math.max(max1[i-1],prices[i-1]-minTmp);
         }
+        int maxTmp = prices[n-1];
+        for (int i=n-1; i>0; i--) {
+            maxTmp = Math.max(maxTmp, prices[i - 1]);
+            max2[i] = Math.max(max2[i+1], maxTmp-prices[i-1]);
+        }
+        int max=0;
         for (int i=1; i<=n; i++) {
-            max = Math.max(max, A[i][n]);
+            max = Math.max(max, max, max1[i]+max2[i]);
         }
         return max;
     }
