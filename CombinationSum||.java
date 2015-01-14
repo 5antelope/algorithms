5ee679f4d7f797f5436380d5a2c82736dbdbebd3
wy4515 <y.wu4515@gmail.com>
2015-01-13 19:15:42 -0500

    speed up!

--- CombinationSum||.java
+++ CombinationSum||.java
@@ -8,18 +8,22 @@ public class Solution {
     }
     public void combination(int[] num, int cp, List<Integer> list, int target) {
         if (target == 0) {
-            if (!lists.contains(list))
-                lists.add(list);
+            // if (!lists.contains(list)) // take extra O(n);
+            lists.add(list);
             return;
         }
         else if (target<0){
             return;
         }
         else {
+            int pre = -1; // all positive nums
             for (int i=cp; i<num.length; i++) {
-                List<Integer> tmp = new ArrayList<Integer>(list);
-                tmp.add(num[i]);
-                combination(num,i+1,tmp,target-num[i]);
+                if (num[i]!=pre) {
+                    List<Integer> tmp = new ArrayList<Integer>(list);
+                    tmp.add(num[i]);
+                    combination(num,i+1,tmp,target-num[i]);
+                    pre = num[i];   // make sure same num. does not appear again at SAME position
+                }
             }
         }
     }
