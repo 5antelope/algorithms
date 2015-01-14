39401e80e0b50405e08bf1f4afe0ea189116d478
wy4515 <y.wu4515@gmail.com>
2015-01-13 20:38:43 -0500

    speed up & AC

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -2,13 +2,19 @@ public class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
         if (n<2)    return 0;
-        int max = 0;
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                int h = Math.min(height[i],height[j]);
-                max = Math.max(max, h*(j-1));
+        int area = 0;
+        int l=0, r=n-1;
+        while (l<r) {
+            if (height[l]<height[r]) {
+                // left limits area, cp cur. max and move limitation (l) for better solution
+                area = Math.max(area, height[l]*(r-l));
+                l++;
+            }
+            else {
+                area = Math.max(area, height[r]*(r-l));
+                r--;
             }
         }
-        return max;
+        return area;
     }
 }
