df7127b42bb716ad2242173f79a571ceda2dcdd6
wy4515 <y.wu4515@gmail.com>
2015-01-14 12:10:23 -0500

    Create WordSearch.java

---
+++ WordSearch.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] flag = new boolean[m][n];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (search(board, word,flag, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+    public boolean search(char[][] board, String word, boolean[][] flag, int i, int j, int cp) {
+        int m = board.length;
+        int n = board[0].length;
+        if (cp == word.length())
+            return true;
+        if (i<0 || j<0 || i>=m || j>=n)
+            return false;
+        if (flag[i][j] == true)
+            return false;
+        if (board[i][j]!=word.charAt(cp))
+            return false;
+        flag[i][j] = true;
+        if (search(board, word, flag, i-1, j, cp+1))
+            return true;
+        if (search(board, word, flag, i+1, j, cp+1))
+            return true;
+        if (search(board, word, flag, i, j+1, cp+1))
+            return true;
+        if (search(board, word, flag, i, j-1, cp+1))
+            return true;
+        flag[i][j] = false;
+        return false;
+    }
+}
