53e3e6997adc3dc86ff9d8749ae46cd9cdb28585
wy4515 <y.wu4515@gmail.com>
2015-01-15 16:16:13 -0500

    Update NextPermutation.java

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,2 +1,9 @@
 /*
+  http://en.wikipedia.org/wiki/Permutation
+
+  1. Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+  2. Find the largest index l greater than k such that a[k] < a[l].
+  3. Swap the value of a[k] with that of a[l].
+  4. Reverse the sequence from a[k + 1] up to and including the final element a[n].
+  --- at which point a[k] < a[k + 1] does not exist, indicating that this is the last permutation.
  */
