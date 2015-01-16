eb2a4066f302ecbfd94d03c948f8d1119254487d
wy4515 <y.wu4515@gmail.com>
2015-01-16 16:54:55 -0500

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -12,4 +12,30 @@ public class Solution {
         }
         return Integer.toBinaryString(num1+num2);
     }
+    /*
+        AC Version
+        1. to it bit by bit
+        2. StringBuilder append at tail;
+            if wanna add at head, concat strings
+        3. a common trick to deal with strings with different length
+    */
+    public String addBinary(String a, String b) {
+        int car = 0;
+        int n1 = a.length();
+        int n2 = b.length();
+        String res = "";
+        for (int i=0; i<Math.max(n1,n2); i++) {
+            int p,q;
+            // a common trick (see also 'Compare Version Number')
+            p = i<n1? a.charAt(n1-1-i)-'0': 0;
+            q = i<n2? b.charAt(n2-1-i)-'0': 0;
+            
+            int sum = p+q+car;
+            car = sum / 2;
+            int r = sum % 2;
+            res = r+res;
+        }
+        if (car==1) return "1"+res;
+        else return res;
+    }
 }
