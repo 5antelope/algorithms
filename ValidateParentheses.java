c73f84c4985b3878e4ab51f46d160552febd5ea0
wy4515 <y.wu4515@gmail.com>
2015-01-16 12:11:04 -0500

    Create ValidateParentheses.java

---
+++ ValidateParentheses.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+    public boolean isValid(String s) {
+        if (s.length()<1) return true;
+        LinkedList<Character> stack = new LinkedList<Character>();
+        for (int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if (c=='{' || c=='[' || c=='(')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())  return false;
+                switch (stack.pop()) {
+                    case '{':
+                        if (c!='}') return false;
+                        break;
+                    case '[':
+                        if (c!=']') return false;
+                        break;
+                    case '(':
+                        if (c!=')') return false;
+                        break;
+                }
+            }
+        }
+        return stack.isEmpty();   // in case '()('
+    }
+}
