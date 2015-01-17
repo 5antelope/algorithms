e7af87a5099fa9c17f88d990ce6b4a2563998d7a
wy4515 <y.wu4515@gmail.com>
2015-01-17 13:55:36 -0500

    Create LongestPalindromicSubstring.java

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n<2)    return s;
+        char[] str = s.toCharArray();
+        int sp = 0, maxLen = 0;
+        boolean[][] table = new boolean[n][n];
+        for (int i=0; i<n; i++) { table[i][i] = true; maxLen = 1; sp = i;}
+        for (int i=0; i<n-1; i++) {
+            if (str[i]==str[i+1]) { table[i][i+1] = true; maxLen = 2; sp = i;}
+        }
+            
+        for (int len=3; len<=n; len++) {
+            for (int i=0; i+len-1<n; i++) {   // (i,i+len-1)
+                if (str[i]==str[i+len-1] && table[i+1][i+len-2]) {
+                    table[i][i+len-1] = true;  maxLen = len; sp = i;
+                }
+            }
+        }
+        return s.substring(sp,sp+maxLen);
+    }
+}
