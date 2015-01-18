c1750567b97c67aaa883bffce819e992e28e2ad6
wy4515 <y.wu4515@gmail.com>
2015-01-18 13:22:15 -0500

    Update reverseListnode.java

--- util/reverseListnode.java
+++ util/reverseListnode.java
@@ -1,5 +1,11 @@
-ListNode pre;
+ListNode pre = head;
 ListNode cur;
 ListNode next;
+pre.next = cur;
 while (cur!=null) {
+  next = cur.next;
+  cur.next = pre;
+  pre = cur;
+  cur = next;
 }
+head.next = null;
