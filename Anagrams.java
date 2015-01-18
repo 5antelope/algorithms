36db5c48b15a84544d9792def859d9b77b3b3b75
wy4515 <y.wu4515@gmail.com>
2015-01-17 22:16:13 -0500

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -3,6 +3,10 @@ public class Solution {
     public List<String> anagrams(String[] strs) {
         int n = strs.length;
         list = new ArrayList<String>();
+        /* 
+            hash - string statistic prob.
+            sorted string as key - a list of unsort string as value
+        */
         HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
         for (int i=0; i<n; i++) {
             String cur = strs[i];
