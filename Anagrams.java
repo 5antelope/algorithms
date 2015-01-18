be12da038a19242c4f37e45d5294b6458ed66599
wy4515 <y.wu4515@gmail.com>
2015-01-17 22:15:06 -0500

    Create Anagrams.java

---
+++ Anagrams.java
@@ -0,0 +1,26 @@
+public class Solution {
+    List<String> list;
+    public List<String> anagrams(String[] strs) {
+        int n = strs.length;
+        list = new ArrayList<String>();
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for (int i=0; i<n; i++) {
+            String cur = strs[i];
+            char[] cs = cur.toCharArray();
+            Arrays.sort(cs);
+            String key = new String(cs);
+            ArrayList<String> l = map.get(key);
+            if (l==null) { l = new ArrayList<String>(); l.add(cur); map.put(key,l);}
+            else l.add(cur);
+        }
+        for (String k:map.keySet()) {
+            ArrayList<String> arr = map.get(k);
+            if (arr.size()<2)   continue;
+            else {
+                for (String s:arr)
+                    list.add(s);
+            }
+        }
+        return list;
+    }
+}
