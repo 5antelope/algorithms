4f47bb3b8054635ab8d06f4e6b15562a5ba81575
wy4515 <y.wu4515@gmail.com>
2015-01-18 21:39:09 -0500

    null point exception.!

---
+++ RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head==null) return null;
+        ListNode tmp = new ListNode(head.val-1);
+        tmp.next = head;
+        ListNode p   = tmp;
+        ListNode newHeader = new ListNode(0);
+        while (p!=null && p.next!=null) {
+            while (p.next!=null && p.val == p.next.val)
+                p = p.next;
+            p = p.next;
+            newHeader.next = p;
+            newHeader = newHeader.next;
+        }
+        return newHeader.next;
+    }
+}
