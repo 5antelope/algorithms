32fe8d45efca77907ad2f1d238c16a16ab1d53ef
wy4515 <y.wu4515@gmail.com>
2015-01-18 22:32:48 -0500

    need goddamn summary about listnodes!!!

--- RemoveDuplicatesfromSortedListII.java
+++ RemoveDuplicatesfromSortedListII.java
@@ -1,16 +1,17 @@
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head==null || head.next==null) return null;
-        ListNode tmp = new ListNode(head.val-1);
-        tmp.next = head;
-        ListNode p   = tmp;
-        ListNode newHeader = new ListNode(0);
-        while (p!=null && p.next!=null) {
-            while (p.next!=null && p.val == p.next.val)
-                p = p.next;
-            p = p.next;
-            newHeader.next = p;
-            newHeader = newHeader.next;
+        if (head==null || head.next==null) return head;
+        ListNode newHeader = new ListNode(head.val-1); // put results
+        ListNode temp = head;
+        ListNode p = newHeader;
+        while (temp!=null) {
+            boolean dup = false;
+            while (temp.next!=null && temp.val==temp.next.val){
+                dup = true;
+                temp = temp.next;
+            }
+            if (!dup)   {p.next = new ListNode(temp.val); p = p.next; temp = temp.next;}
+            else temp = temp.next;
         }
         return newHeader.next;
     }
