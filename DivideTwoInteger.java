5092e18767b37f9b4b643e7a2999e24ffeabcebb
wy4515 <y.wu4515@gmail.com>
2015-01-24 12:57:06 -0500

    math.. boundary issue!

---
+++ DivideTwoInteger.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor==0) return Integer.MAX_VALUE;
+        if (divisor==1) return dividend;
+        int res = 0;
+        if(dividend == Integer.MIN_VALUE) {
+            dividend += Math.abs(divisor);
+            if(divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+            res++;
+        }
+        if(divisor == Integer.MIN_VALUE)    return res;  
+        
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int digit = 0;
+        while (a>=b) {
+            b = b<<1;
+            digit++;
+        }
+        while (digit>=0 && a>0) {
+            if (a>=b) {
+                res += 1<<digit;
+                a = a-b;
+            }
+            b = b>>1;
+            digit--;
+        }
+        return (dividend^divisor)>>>31==1?-res:res;
+    }
+}
