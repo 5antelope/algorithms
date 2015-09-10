ba8ad46ea4d6ad6ccb1c97c7190cc9ff9d67fbc8
wy4515 <y.wu4515@gmail.com>
2015-09-10 16:17:39 -0400

    Update TreeTraversal.java

--- util/TreeTraversal.java
+++ util/TreeTraversal.java
@@ -51,22 +51,49 @@ public void inorder(TreeNode root) {
 }
 
 public void postorder(TreeNode root) {
-  LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
-  TreeNode cur = null;
+  if(root == null)
+      return; 
+
+  Stack<TreeNode> stack = new Stack<TreeNode>();
   stack.push(root);
-  TreeNode pre = null;  // last pushed node
-  while (!stack.isEmpty()) {
-      cur = stack.peek();
-      if (cur.left==null && cur.right==null 
-        || cur.left==pre && pre!=null 
-        || cur.right==pre && pre!=null ) {
-          // mark as visited
-          pre = cur;
-          System.out.println(stack.pop().val);
+
+  TreeNode prev = null;
+  while(!stack.empty()){
+      TreeNode curr = stack.peek();
+
+      // go down the tree.
+      //check if current node is leaf, if so, process it and pop stack,
+      //otherwise, keep going down
+      if(prev == null || prev.left == curr || prev.right == curr){
+          //prev == null is the situation for the root node
+          if(curr.left != null){
+              stack.push(curr.left);
+          }else if(curr.right != null){
+              stack.push(curr.right);
+          }else{
+              stack.pop();
+              System.out.println(curr.val);
+          }
+
+      //go up the tree from left node    
+      //need to check if there is a right child
+      //if yes, push it to stack
+      //otherwise, process parent and pop stack
+      }else if(curr.left == prev){
+          if(curr.right != null){
+              stack.push(curr.right);
+          }else{
+              stack.pop();
+              System.out.println(curr.val);
+          }
+
+      //go up the tree from right node 
+      //after coming back from right node, process parent node and pop stack. 
+      }else if(curr.right == prev){
+          stack.pop();
+          System.out.println(curr.val);
       }
-      else {
-          if (cur.right!=null)  stack.push(cur.right);
-          if (cur.left!=null)  stack.push(cur.left);
-      }
+
+      prev = curr;
   }
 }
